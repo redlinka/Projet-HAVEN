f3979bb14c3149ccc92bbb757ec04481a554d843
Enzo Milic <enzo.milic@orange.fr>
2026-02-24 18:37:05 +0100

    new depart

--- README.md
+++
@@ -1,2 +0,0 @@
-# Projet-HAVEN
-A follow up of the TableauLEGO project, now implementing two new games as well as a mongoDB backend.
--- TableauLEGO/java/src/main/java/fr/uge/univ_eiffel/mediators/security/BrickVerifier.java
+++
@@ -1,7 +0,0 @@
-package fr.uge.univ_eiffel.mediators.security;
-
-import fr.uge.univ_eiffel.mediators.Brick;
-
-public sealed interface BrickVerifier permits OfflineVerifier, OnlineVerifier{
-    boolean verify(Brick brick);
-}

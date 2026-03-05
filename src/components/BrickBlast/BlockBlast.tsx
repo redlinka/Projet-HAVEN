786a7af022316d18622037734226d40f47d21b26
Nolann maunier <nolann.maunier@proton.me>
2026-03-05 23:31:37 +0100

    + added 3 blocks generation & random color
    the 3 blocks are now appearing inside the block holder. coordinates may change with the final shapes of the blocks

--- src/components/BrickBlast/BlockBlast.tsx
+++ src/components/BrickBlast/BlockBlast.tsx
@@ -1,39 +1,47 @@
-import {Canvas} from "@react-three/fiber"
-import {Suspense, useEffect, useRef, useState} from "react";
-import {Background, Grid, BlockHolder, CameraController, Score} from "./SceneElements.tsx";
-import Block from "./Block.tsx";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useRef, useState } from "react";
+import {
+  Background,
+  Grid,
+  BlockHolder,
+  CameraController,
+  Score,
+} from "./SceneElements.tsx";
+import BlocksGeneration from "./Block.tsx";
 
 export interface GameData {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 }
 
 export const BlockScene = () => {
-    const [fov, setFov] = useState(80);
-    const sharedData = useRef<GameData>({ x: 0, y: 0 });
+  const [fov, setFov] = useState(80);
+  const sharedData = useRef<GameData>({ x: 0, y: 0 });
 
-    useEffect(() => {
-        const update = () => {
-            setFov(window.innerWidth < window.innerHeight ? 100 : 80);
-        };
-        update();
-        window.addEventListener("resize", update);
-        return () => window.removeEventListener("resize", update);
-    }, []);
+  useEffect(() => {
+    const update = () => {
+      setFov(window.innerWidth < window.innerHeight ? 100 : 80);
+    };
+    update();
+    window.addEventListener("resize", update);
+    return () => window.removeEventListener("resize", update);
+  }, []);
 
-    return (
-        <div style={{ width: "100vw", height: "100vh" }}>
-            <Canvas camera={{ fov: fov, near: 0.1, far: 1000, position: [0, -15, 80]}}>
-                <CameraController />
-                <Suspense fallback={null}>
-                    <Score data={sharedData} />
-                    <Grid data={sharedData} />
-                    <BlockHolder />
-                    <Block />
-                    <Background />
-                </Suspense>
-            </Canvas>
-        </div>
-    )
-}
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <Canvas
+        camera={{ fov: fov, near: 0.1, far: 1000, position: [0, -15, 80] }}
+      >
+        <CameraController />
+        <Suspense fallback={null}>
+          <Score data={sharedData} />
+          <Grid data={sharedData} />
+          <BlockHolder />
+          <BlocksGeneration />
+          <Background />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
 export default BlockScene;

f331054e5ab1d9d942b759fccaeeb7e981bd5f87
Kwyvo <kwyvo9@gmail.com>
2026-03-07 17:44:23 +0100

    connection between atlas and nodejs done

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv';
+import connectDB from './db';
+import Player from './models/Player';
+import jwt from 'jsonwebtoken';
+
+
+const test = async () => {
+
+  const token = jwt.sign(
+      { linkedPHPId: null },
+      process.env.JWT_SECRET as string,
+      { expiresIn: '1d' }
+    );
+
+  const player = new Player({
+    isGuest: true,
+    linkedPHPId: null,
+    sessionToken: token,
+    lastConnectedAt: new Date(),
+    games: [
+      {
+        game: 'PUZZLE',
+        playedAt: new Date(),
+        mode: 'solo',
+        points: 50,
+        earnedAt: new Date(),
+        expiresAt: new Date('2026-04-07'),
+        used: false
+      }
+    ]
+  });
+
+  await player.save();
+  console.log('Joueur inséré', player);
+};
+
+
+dotenv.config();
+
+
+// MAIN
+const main = async() => {
+  await connectDB();
+  await test();
+}
+
+main();
---
+++ src/models/Player.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from 'mongoose';
+
+const gameSchema = new Schema({
+    game: { type: String, required: true },
+    playedAt: { type: Date, required: true },
+    mode: { type: String, enum: ['solo', 'duplicate'], required: true },
+    points: { type: Number, required: true },
+    earnedAt: { type: Date, required: true },
+    expiresAt: { type: Date, required: true },
+    used: { type: Boolean, default: false }
+});
+
+const playerSchema = new Schema({
+    isGuest: { type: Boolean, default: true },
+    linkedPHPId: { type: String, default: null },
+    sessionToken: { type: String, default: null },
+    lastConnectedAt: { type: Date },
+    games: [gameSchema]
+});
+
+playerSchema.index({ 'linkedPHPId': 1 });
+playerSchema.index({ 'games.expiresAt': 1 });
+playerSchema.index({ sessionToken: 1 });
+playerSchema.index({ lastConnectedAt: 1 }, { expireAfterSeconds: 31536000 });
+
+const Player = mongoose.model('Player', playerSchema,'Players');
+export default Player;

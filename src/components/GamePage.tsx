be4ac59243220c2901f29e80ffe4668c0607e49c
Draken <held.esteves@gmail.com>
2026-03-14 14:41:42 +0100

    Add chat popup for phone

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -4,8 +4,13 @@ import Chatter from "./Chat/Chatter";
 import type { Game } from "../types/types";
 
 import "../styles/components/GamePage.css";
+import Button from "./commons/Button";
+import { MessageCircle, X } from "lucide-react";
+import { useState } from "react";
+import { createPortal } from "react-dom";
 
 export default function GamePage({ games }: { games: Game[] }) {
+  const [showChat, setShowChat] = useState<boolean>(false);
   const { id } = useParams();
 
   const gameSelected = id ? games[Number(id)] : null;
@@ -22,6 +27,24 @@ export default function GamePage({ games }: { games: Game[] }) {
       <div className="game-container-right">
         <Chatter />
       </div>
+      {window.innerWidth < 700 && (
+        <button
+          onClick={() => setShowChat((prev) => !prev)}
+          className="chat-button"
+        >
+          {!showChat ? <MessageCircle /> : <X />}
+        </button>
+      )}
+
+      {showChat &&
+        createPortal(
+          <div className="bg-filter">
+            <div className="chat-popup">
+              <Chatter />
+            </div>
+          </div>,
+          document.body,
+        )}
     </div>
   );
 }

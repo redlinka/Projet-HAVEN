fc7fa3fd00f93d7a6c401f93da17988d5ff0c02f
redlinka <nachnouchiadam@gmail.com>
2026-03-14 16:38:02 +0100

    enabled debug messages

--- TableauLEGO/Php/img2brick_final/includes/footer.php
+++ TableauLEGO/Php/img2brick_final/includes/footer.php
@@ -1,6 +1,6 @@
 <footer style="position: inline-block; text-align: center; background-color: var(--main-brown); width: 100%; padding: 1vh 0 0 0; bottom: 0;">
     <p>
-        <a href="https://github.com/redlinka/TableauLEGO/tree/dev/Php" target="_blank" rel="noopener noreferrer" style="color: #FFFFFF; text-decoration: none; font: 500 clamp(13px, 1.5vw, 18px) inter, sans-serif;" data-i18n="footer.github">
+        <a href="https://github.com/redlinka/Projet-HAVEN/tree/new-design" target="_blank" rel="noopener noreferrer" style="color: #FFFFFF; text-decoration: none; font: 500 clamp(13px, 1.5vw, 18px) inter, sans-serif;" data-i18n="footer.github">
             View on GitHub
         </a>
     </p>

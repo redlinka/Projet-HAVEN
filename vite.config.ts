411acdaf90c8b1629cdee6d6622a9c92bd0e33f8
redlink <nachnouchiadam@gmail.com>
2026-04-02 23:16:29 +0200

    link with serv

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-   proxy: {
-  "/api-node": {
-    target: "http://localhost:2025",
-    changeOrigin: true,
-  }
-}
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // If running 'npm run build', use '/games/'. If running locally, use root '/'
+    base: command === 'build' ? '/games/' : '/',
+
+    server: {
+      proxy: {
+        "/api-node": {
+          target: "http://127.0.0.1:2025",
+          changeOrigin: true,
+        }
+      }
+    }
   }
 })
+
 
